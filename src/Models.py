5821452e3fcec37f85e7eab54ca5c2957b8084c6
FishingOnATree <raysung78@gmail.com>
2016-07-13 11:42:43 +0800

    Refactored to have model takes control on the training part

--- src/Models.py
+++ src/Models.py
@@ -2,9 +2,12 @@ __author__ = 'Rays'
 # from keras.models import Sequential
 # from keras.layers import Dense, Dropout, Activation
 # from keras.optimizers import SGD
+
+import timeit
+
 import numpy as np
-from sklearn import svm, datasets, feature_selection, cross_validation
 import matplotlib.pyplot as plt
+from sklearn import svm, feature_selection, cross_validation
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import GaussianNB
@@ -13,11 +16,14 @@ from sklearn.pipeline import Pipeline
 
 class ModelBase:
 
-    def __init__(self, settings):
-        self.model = self.get_model(settings)
-        self.settings = settings
+    def __init__(self):
+        self.model = None
+        pass
 
-    def get_model(self, settings):
+    def init_model(self, settings):
+        raise NotImplementedError("Class %s doesn't implement aMethod()" % __name__)
+
+    def get_settings(self):
         raise NotImplementedError("Class %s doesn't implement aMethod()" % __name__)
 
     def train(self, x_train, y_train, x_cv, y_cv):
@@ -26,35 +32,96 @@ class ModelBase:
     def predict(self, x):
         raise NotImplementedError("Class %s doesn't implement aMethod()" % __name__)
 
-    def plot_feature_selection_diagram(self, x, y):
-        clf = Pipeline([('selector', self.get_feature_selection()), ('learning', self.model)])
-        score_means = list()
-        score_stds = list()
-        percentiles = (10, 20, 30, 40, 50, 60, 70)
-        for percentile in percentiles:
-            clf.set_params(selector__percentile=percentile)
-            # Compute cross-validation score using all CPUs
-            this_scores = cross_validation.cross_val_score(clf, x, y, n_jobs=1)
-            score_means.append(this_scores.mean())
-            score_stds.append(this_scores.std())
-            print("Finished percentile %d" % percentile)
-        plt.errorbar(percentiles, score_means, np.array(score_stds))
+    def update_model(self, settings):
+        self.model = self.init_model(settings)
 
+    def plot_feature_selection_diagram(self, x, y):
+        for index, setting in enumerate(self.get_settings()):
+            self.update_model(setting)
+            clf = Pipeline([('selector', self.get_feature_selection()), ('learning', self.model)])
+            score_means = list()
+            score_stds = list()
+            percentiles = (10, 25, 40, 55, 70, 85)
+            for percentile in percentiles:
+                clf.set_params(selector__percentile=percentile)
+                # Compute cross-validation score using all CPUs
+                this_scores = cross_validation.cross_val_score(clf, x, y, n_jobs=1)
+                score_means.append(this_scores.mean())
+                score_stds.append(this_scores.std())
+                print("Finished percentile %d" % percentile)
+            plt.errorbar(percentiles, score_means, np.array(score_stds), label="Setting #{}".format(index))
         plt.title('Performance of the SVM-Anova varying the percentile of features selected')
         plt.xlabel('Percentile')
         plt.ylabel('Prediction rate')
-
         plt.axis('tight')
+        plt.legend()
         plt.show()
+
+    def run(self, x_train, x_cv, y_train, y_cv):
+        stats_list = []
+        h_train = None
+        h_cv = None
+        for setting in self.get_settings():
+            self.update_model(setting)
+            start_time = timeit.default_timer()
+            self.train(x_train, y_train, x_cv, y_cv)
+            total_time = timeit.default_timer() - start_time
+            print("Training finished with ", setting, " in ", total_time, " sec")
+            # record settings
+            setting["run_time"] = total_time
+            train_stats, h_train = self.validate_prediction(x_train, y_train, setting, "training")
+            stats_list.append(train_stats)
+            cv_stats, h_cv = self.validate_prediction(x_cv, y_cv, setting, "cv")
+            stats_list.append(cv_stats)
+        return stats_list, h_train, h_cv
+
+    def validate_prediction(self, x_data, y_data, settings, traing_type):
+        h_train = self.model.predict(x_data)
+        train_stats = self.calculate_metrics(y_data, h_train)
+        train_stats["type"] = traing_type
+        train_stats.update(settings)
+        print("%s stats: " % traing_type)
+        print("Accuracy = %3.2f%%" % (train_stats["accuracy"]))
+        print("%2.2f%% bad loans predicted correctly" % (train_stats["false_accuracy"]))
+        return train_stats, h_train
+
+    def get_param_headers(self):
+        headers = list(["model", "type", "accuracy", "false_accuracy", "tp", "tn", "fp", "fn",
+                        "precision", "recall", "run_time"])
+        headers.extend(self.get_model_param_headers())
+        return headers
+
+    @staticmethod
+    def get_model_param_headers():
+        raise NotImplementedError("Class %s doesn't implement aMethod()" % __name__)
+
+    @staticmethod
+    def calculate_metrics(y, h):
+        size = len(y)
+        error_count = 0.0
+        count_dict = {}
+        for i in range(size):
+            if tuple((y[i], h[i])) not in count_dict.keys():
+                count_dict[tuple((y[i], h[i]))] = 0
+            count_dict[tuple((y[i], h[i]))] += 1
+            error_count += 0 if y[i] == h[i] else 1
+        accuracy = float(size - error_count) / size * 100
+        true_pos = count_dict.get(tuple((1.0, 1.0)), 0)
+        true_neg = count_dict.get(tuple((0.0, 0.0)), 0)
+        false_pos = count_dict.get(tuple((0.0, 1.0)), 0)
+        false_neg = count_dict.get(tuple((1.0, 0.0)), 0)
+        return {"tp": true_pos,
+                "tn": true_neg,
+                "fp": false_pos,
+                "fn": false_neg,
+                "accuracy": accuracy,
+                "false_accuracy": 0 if true_neg == 0 else true_neg*100.0/(true_neg + false_pos),
+                "precision": 0 if true_pos + false_pos == 0 else true_pos * 100.0 / (true_pos + false_pos),
+                "recall": 0 if true_pos + false_neg == 0 else true_pos * 100.0 / (true_pos + false_neg)}
 
     @staticmethod
     def get_feature_selection():
         return feature_selection.SelectPercentile(feature_selection.f_classif)
-
-    @staticmethod
-    def get_param_headers():
-        raise NotImplementedError("Class %s doesn't implement aMethod()" % __name__)
-
 
 # class NeuralNetworkModel(ModelBase):
 #
@@ -109,8 +176,12 @@ class ModelBase:
 
 class AdaBoostModel(ModelBase):
 
-    def get_model(self, settings):
+    def init_model(self, settings):
         return AdaBoostClassifier(n_estimators=settings["n_estimators"])
+
+    def get_settings(self):
+        return [{"n_estimators": 10},
+                {"n_estimators": 20}]
 
     def train(self, x_train, y_train, x_cv, y_cv):
         self.model.fit(x_train, y_train)
@@ -119,18 +190,44 @@ class AdaBoostModel(ModelBase):
         return self.model.predict(x)
 
     @staticmethod
-    def get_param_headers():
+    def get_model_param_headers():
         return ["n_estimators"]
+
+
+class GaussianNBModel(ModelBase):
+
+    def init_model(self, settings):
+        return GaussianNB()
+
+    def get_settings(self):
+        return [{}]
+
+    def train(self, x_train, y_train, x_cv, y_cv):
+        self.model.fit(x_train, y_train)
+
+    def predict(self, x):
+        return self.model.predict(x)
+
+    @staticmethod
+    def get_model_param_headers():
+        return []
 
 
 class RandomForestModel(ModelBase):
 
-    def get_model(self, settings):
+    def init_model(self, settings):
         return RandomForestClassifier(n_estimators=settings["n_estimators"],
                                       min_samples_split=settings["min_samples_split"],
                                       min_samples_leaf=settings["min_samples_leaf"],
                                       class_weight=settings["class_weight"])
 
+    def get_settings(self):
+        return [{"n_estimators": n_est, "min_samples_split": min_ss, "min_samples_leaf": min_sl, "class_weight": cw}
+                for n_est in [10, 30, 50]
+                for min_ss in [5, 20]
+                for min_sl in [2, 10]
+                for cw in [{0: 0.7, 1: 0.3}, {0: 0.88, 1: 0.12}]]
+
     def train(self, x_train, y_train, x_cv, y_cv):
         self.model.fit(x_train, y_train)
 
@@ -138,33 +235,22 @@ class RandomForestModel(ModelBase):
         return self.model.predict(x)
 
     @staticmethod
-    def get_param_headers():
+    def get_model_param_headers():
         return ["n_estimators", "min_samples_split", "min_samples_leaf", "class_weight"]
-
-
-class GaussianNBModel(ModelBase):
-
-    def get_model(self, settings):
-        return GaussianNB()
-
-    def train(self, x_train, y_train, x_cv, y_cv):
-        self.model.fit(x_train, y_train)
-
-    def predict(self, x):
-        return self.model.predict(x)
-
-    @staticmethod
-    def get_param_headers():
-        return []
 
 
 class SVMModel(ModelBase):
 
-    def get_model(self, settings):
+    def init_model(self, settings):
         return svm.SVC(kernel="rbf",
                        C=settings["C"],
+                       max_iter=settings["max_iter"],
                        class_weight=settings["class_weight"],
                        gamma=settings["gamma"])
+
+    def get_settings(self):
+        return [{"C": 1, "max_iter": -1, "class_weight": {0: 0.88, 1: 0.12}, "gamma": 1},
+                {"C": 1, "max_iter": -1, "class_weight": {0: 0.5, 1: 0.5}, "gamma": 1}]
 
     def train(self, x_train, y_train, x_cv, y_cv):
         self.model.fit(x_train, y_train)
@@ -173,21 +259,19 @@ class SVMModel(ModelBase):
         return self.model.predict(x)
 
     @staticmethod
-    def get_param_headers():
+    def get_model_param_headers():
         return ["C", "max_iter", "class_weight", "gamma"]
 
 
+MODEL_DICT = {"ADA": AdaBoostModel,
+              "GB": GaussianNBModel,
+              "RF": RandomForestModel,
+              "SVM": SVMModel}
+#"NN": NeuralNetworkModel
 
-MODEL_DICT = { #"NN": NeuralNetworkModel,
-               "ADA": AdaBoostModel,
-               "GB": GaussianNBModel,
-               "RF": RandomForestModel,
-               "SVM": SVMModel}
 
-
-def get_instance(model_name, settings):
+def get_instance(model_name):
     if model_name not in MODEL_DICT:
         raise ValueError("%s is not a valid options in %s" % (model_name, str(MODEL_DICT.keys())))
     else:
-        model_class = MODEL_DICT[model_name]
-        return model_class(settings)
+        return MODEL_DICT[model_name]()
--- src/Trainer.py
+++ src/Trainer.py
@@ -1,9 +1,7 @@
 from __future__ import print_function
 import datetime
-import numpy as np
 import random
 import time
-import timeit
 import sys
 
 from sklearn import preprocessing
@@ -24,40 +22,6 @@ def data_preprocess(df_train, df_cv, df_test):
     x_cv = scaler.transform(x_cv)
     x_test = scaler.transform(x_test)
     return x_train, x_cv, x_test, y_train, y_cv, y_test
-
-
-def cal_accuracy(y, h):
-    size = len(y)
-    error_count = 0.0
-    count_dict = {}
-    for i in range(size):
-        if tuple((y[i], h[i])) not in count_dict.keys():
-            count_dict[tuple((y[i], h[i]))] = 0
-        count_dict[tuple((y[i], h[i]))] += 1
-        error_count += 0 if y[i] == h[i] else 1
-    accuracy = float(size - error_count) / size * 100
-    true_pos = count_dict[tuple((1.0, 1.0))] if tuple((1.0, 1.0)) in count_dict.keys() else 0
-    true_neg = count_dict[tuple((0.0, 0.0))] if tuple((0.0, 0.0)) in count_dict.keys() else 0
-    false_pos = count_dict[tuple((0.0, 1.0))] if tuple((0.0, 1.0)) in count_dict.keys() else 0
-    false_neg = count_dict[tuple((1.0, 0.0))] if tuple((1.0, 0.0)) in count_dict.keys() else 0
-    bad_loan_accuracy = 0 if true_neg == 0 else true_neg*100/float(true_neg+false_pos)
-    return {"tp": true_pos,
-            "tn": true_neg,
-            "fp": false_pos,
-            "fn": false_neg,
-            "accuracy": accuracy,
-            "false_accuracy": bad_loan_accuracy}
-
-
-def validate_prediction(model, x_data, y_data, settings, traing_type):
-    h_train = model.predict(x_data)
-    train_stats = cal_accuracy(y_data, h_train)
-    train_stats["type"] = traing_type
-    train_stats.update(settings)
-    print("%s stats: " % traing_type)
-    print("Accuracy = %3.2f%%" % (train_stats["accuracy"]))
-    print("%2.2f%% bad loans predicted correctly" % (train_stats["false_accuracy"]))
-    return train_stats, h_train
 
 
 def map_random(x):
@@ -91,56 +55,22 @@ def get_separated_data_and_frame(data_f):
     print(df_train.shape[0], df_cv.shape[0], df_test.shape[0])
     # need to normalize mean and standardization
     x_train, x_cv, x_test, y_train, y_cv, y_test = data_preprocess(df_train, df_cv, df_test)
-    return df_train, df_cv, df_test,  x_train,x_cv, x_test, y_train, y_cv, y_test
+    return df_train, df_cv, df_test,  x_train, x_cv, x_test, y_train, y_cv, y_test
 
 
-def train(data_f, model_name):
-    df_train, df_cv, df_test,  x_train,x_cv, x_test, y_train, y_cv, y_test = get_separated_data_and_frame(data_f)
-
-    print("Data size: Training, CV, Test = %d, %d, %d" % (x_train.shape[0], x_cv.shape[0], x_test.shape[0]))
-    stats_list = []
-    model = None
-    for iteration in [[1, {0: 0.88, 1: 0.12}]]:
-                c = iteration[0]
-                class_weights = iteration[1]
-                max_iter = -1
-                # TODO: figure out a better way to specify different parameters for each model.
-                settings = {"C": c,
-                            "max_iter": max_iter,
-                            "cache_size": 10000,
-                            "class_weight": class_weights,
-                            "gamma": 1,
-                            "min_samples_split": 12,
-                            "min_samples_leaf": 5,
-                            "n_estimators": 50,
-                            "model": model_name}
-                model = Models.get_instance(model_name, settings)
-                start_time = timeit.default_timer()
-                model.train(x_train, y_train, x_cv, y_cv)
-                total_time = timeit.default_timer() - start_time
-                print("Training finished with ", settings, " in ", total_time, " sec")
-                # record settings
-                settings["run_time"] = total_time
-                train_stats, h_train = validate_prediction(model, x_train, y_train, settings, "training")
-                # df_train["prediction"] = np.copy(h_train)
-                stats_list.append(train_stats)
-                cv_stats, h_cv = validate_prediction(model, x_cv, y_cv, settings, "cv")
-                # df_cv["prediction"] = np.copy(h_cv)
-                stats_list.append(cv_stats)
-                test_stats, h_test = validate_prediction(model, x_test, y_test, settings, "test")
-                df_test["prediction"] = np.copy(h_test)
-                stats_list.append(test_stats)
-
-                # df_test.to_csv(config.data_dir+"/df_test.csv")
-                # # save final weight
-                # weight_f = config.data_dir + "/weights/" + name_file(settings)
-                # joblib.dump(model, weight_f)
-    headers = ["model", "type", "accuracy", "false_accuracy", "tp", "tn", "fp", "fn", "run_time"]
-    headers.extend(model.get_param_headers())
-
+def train(data_f, model, model_name):
+    df_train, df_cv, df_test,  x_train, x_cv, x_test, y_train, y_cv, y_test = get_separated_data_and_frame(data_f)
+    print("Data size: Training, CV, Test = %s, %s, %s" % (str(x_train.shape), str(x_cv.shape), str(x_test.shape)))
+    print("Label size: Training, CV, Test = %s, %s, %s" % (str(y_train.shape), str(y_cv.shape), str(y_test.shape)))
+    stats_list, h_train, h_cv = model.run(x_train, x_cv, y_train, y_cv)
     time_str = get_time_str()
     out_put_fn = config.data_dir + "/" + model_name + "_" + time_str + ".csv"
-    LCUtil.save_results(headers, stats_list, out_put_fn)
+    print(model.get_param_headers())
+    LCUtil.save_results(model.get_param_headers(), stats_list, out_put_fn)
+    # df_test.to_csv(config.data_dir+"/df_test.csv")
+    # # save final weight
+    # weight_f = config.data_dir + "/weights/" + name_file(settings)
+    # joblib.dump(model, weight_f)
     return df_train, df_cv, df_test, model
 
 
@@ -153,27 +83,13 @@ def predict(data_f, model):
     scaler = preprocessing.StandardScaler().fit(x)
     x_norm = scaler.transform(x)
     h = model.predict(x_norm)
-    df["prediction"] = np.copy(h)
+    # TODO: need a better way to copy: df["prediction"] = np.copy(h)
     return df
 
 
-def feature_selection(data_f, model_name):
+def feature_selection(data_f, model):
     df_train, df_cv, df_test,  x_train,x_cv, x_test, y_train, y_cv, y_test = get_separated_data_and_frame(data_f)
-    for iteration in [[1, {0: 0.75, 1: 0.25}]]:
-        c = iteration[0]
-        class_weights = iteration[1]
-        max_iter = -1
-        settings = {"C": c,
-                    "max_iter": max_iter,
-                    "cache_size": 10000,
-                    "class_weight": class_weights,
-                    "gamma": 1,
-                    "min_samples_split": 12,
-                    "min_samples_leaf": 5,
-                    "n_estimators": 50,
-                    "model": model_name}
-        model = Models.get_instance(model_name, settings)
-        model.plot_feature_selection_diagram(x_train, y_train)
+    model.plot_feature_selection_diagram(x_train, y_train)
 
 
 def main():
@@ -181,14 +97,17 @@ def main():
         print('not enough arguments')
         sys.exit()
     else:
+        purpose = sys.argv[1]
         model_name = sys.argv[2]
         # use sample data by default
         data_file = config.data_dir + "/" + sys.argv[3]
 
-        if sys.argv[1] == "train":
-            train(data_file, model_name)
-        elif sys.argv[1] == "feature_select":
-            feature_selection(data_file, model_name)
+        if purpose == "train":
+            model = Models.get_instance(model_name)
+            train(data_file, model, model_name)
+        elif purpose == "feature_selection":
+            model = Models.get_instance(model_name)
+            feature_selection(data_file, model)
         else:
             if len(sys.argv) < 5:
                 print('not enough arguments')
